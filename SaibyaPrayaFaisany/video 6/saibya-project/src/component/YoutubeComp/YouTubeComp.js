10730f8ef69ae6a69b9f38f5ac2065bf8d4ed68b
saibyaprayaf <saibyaprayaf@gmail.com>
2023-11-28 21:00:23 +0700

    vid 4,5 dan 6

---
+++ SaibyaPrayaFaisany/video 6/saibya-project/src/component/YoutubeComp/YouTubeComp.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const YoutuBeComp = () => {
+    return (
+        <div className="youtube-wrapper">
+            <div className="img-thumb">
+                <img src=""alt=""/>
+            </div>
+            <p className="title">title here</p>
+            <p className="desc">desc here</p>
+        </div>
+    )
+}
+
+export default YoutuBeComp;
